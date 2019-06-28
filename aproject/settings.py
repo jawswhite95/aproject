1d2d15ec68b39116b84dce8ab50f126915591051
jawswhite95 <jawswhite95@naver.com>
2019-06-28 15:31:35 +0900

    settings.py*

--- aproject/settings.py
+++ aproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '4lvxd+ge+dlkjag*n*pp^amp*$h$d*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
